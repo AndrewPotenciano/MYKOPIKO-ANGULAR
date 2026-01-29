3d1f40780a877c9a961fba8c3ec9af3005293bce
JamesPotenciano <leynandrew@gmail.com>
2026-01-29 16:59:09 +0800

    price near the name item

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit, input, inject } from '@angular/core';
+import { Component, HostListener, OnInit, input, inject, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { CarouselItem } from '../../models/carousel-item.model';
@@ -12,31 +12,54 @@ let carouselCounter = 0;
 	templateUrl: './carousel.component.html',
 	styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit {
-	
+export class CarouselComponent implements OnInit, AfterViewInit {
 	title = input<string>('Popular Menu');
 	items = input<CarouselItem[]>([]);
 	showDownHint = input(false);
 	showSeparator = input(true);
 	reveal = input(false);
 	carouselId: string = `productsCarousel${++carouselCounter}`;
-	private isMobile = false;
+	isMobile = false;
 
 	public cart = inject(CartService);
 	toastOpen = false;
 	toastMessage = '';
+	showSwipeHint = true;
+
+	@ViewChild('carouselRow') carouselRow?: ElementRef<HTMLDivElement>;
 
 	ngOnInit(): void {
 		this.updateViewport();
 	}
 
+	ngAfterViewInit(): void {
+		this.attachScrollListener();
+	}
+
 	@HostListener('window:resize')
 	onResize(): void {
 		this.updateViewport();
+		this.attachScrollListener();
 	}
 
 	private updateViewport(): void {
 		this.isMobile = typeof window !== 'undefined' && window.innerWidth <= 767;
+	}
+
+	private attachScrollListener(): void {
+		if (this.isMobile && this.carouselRow) {
+			const element = this.carouselRow.nativeElement;
+			element.addEventListener('scroll', () => this.onScroll(element));
+		}
+	}
+
+	private onScroll(element: HTMLDivElement): void {
+		const scrollLeft = element.scrollLeft;
+		const scrollWidth = element.scrollWidth;
+		const clientWidth = element.clientWidth;
+		
+		// Hide swipe hint when near the end (within 50px)
+		this.showSwipeHint = scrollLeft < scrollWidth - clientWidth - 50;
 	}
 
 	addToCart(item: CarouselItem): void {
