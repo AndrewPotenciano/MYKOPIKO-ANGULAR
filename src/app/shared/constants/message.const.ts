aa73d790d08d4958b0aa23612df14e57b59a1b99
AndrewPotenciano <5mgaming72@gmail.com>
2026-02-05 11:54:33 +0800

    fix:labels and messages const fix

--- src/app/shared/constants/message.const.ts
+++ src/app/shared/constants/message.const.ts
@@ -1,38 +1,32 @@
 export const MESSAGES = {
-  HOMEPAGE: {
-    HERO_TITLE: 'Start Your Day',
-    HERO_SUBTITLE: 'With The Perfect Coffee',
-    HERO_DESCRIPTION:
-      'Experience the perfect blend of rich, aromatic coffee and cozy, inviting moments that brighten your day, one cup at a time.',
-    ABOUT_US_DESC: 'Mykopiko serves rich, comforting coffee made with quality beans and brewed to perfection for every coffee lover.',
-    WHY_CHOOSE_US_DESC:
-      'We use only the finest coffee beans, carefully selected from trusted farms to ensure every cup delivers a rich, balanced, and unforgettable flavor.',
-    CONTACT_US_SUB: 'We’d love to hear from you.',
-  },
-  MENU: {
-    HEADER_TITLE: 'Experience the Art of Coffee',
-    HEADER_SUBTITLE: 'Brewed to perfection, served with passion.',
-  },
-  PAYMENT: {
-    SELECT_PAYMENT: 'Select Payment Method',
-    SCAN_TO_PAY: 'Scan to Pay',
-    SCAN_GCASH: 'Scan the QR code below using your Gcash app',
-    SCAN_MAYA: 'Scan the QR code below using your Maya app',
-    PAYMENT_TIP: 'Your order will be processed immediately after payment confirmation.',
-  },
-  FINISH: {
-    THANK_YOU: 'Thank you For Your Order',
-    ARRIVING_SOON: 'Your delicious coffee will arrive soon!',
-    ESTIMATED_DELIVERY: 'Estimated Delivery : 20–30 mins',
-  },
-  TRACK: {
-    TRACK_SUB: 'You can now track the status of your delivery here',
-  },
-  LOGIN: {
-    DESCRIPTION: 'Buy Coffees, access your orders, track deliveries, and enjoy exclusive member perks.',
-    SIGN_IN: 'Sign in to continue',
-    TERMS: 'By continuing, you agree to our Terms & Privacy Policy.',
-    CONTINUE_FACEBOOK: 'Continue With Facebook',
-    CONTINUE_GOOGLE: 'Continue With Google',
-  },
+  HOMEPAGE_HERO_TITLE: 'Start your day',
+  HOMEPAGE_HERO_SUBTITLE: 'with the perfect coffee',
+  HOMEPAGE_HERO_DESCRIPTION:
+    'Experience the perfect blend of rich, aromatic coffee and cozy, inviting moments that brighten your day, one cup at a time.',
+  HOMEPAGE_ABOUT_US_DESC:
+    'Mykopiko serves rich, comforting coffee made with quality beans and brewed to perfection for every coffee lover.',
+  HOMEPAGE_WHY_CHOOSE_US_DESC:
+    'We use only the finest coffee beans, carefully selected from trusted farms to ensure every cup delivers a rich, balanced, and unforgettable flavor.',
+  HOMEPAGE_CONTACT_US_SUB: 'We’d love to hear from you.',
+
+  MENU_HEADER_TITLE: 'Experience the Art of Coffee',
+  MENU_HEADER_SUBTITLE: 'Brewed to perfection, served with passion.',
+
+  PAYMENT_SELECT_PAYMENT: 'Select Payment Method',
+  PAYMENT_SCAN_TO_PAY: 'Scan to Pay',
+  PAYMENT_SCAN_GCASH: 'Scan the QR code below using your Gcash app',
+  PAYMENT_SCAN_MAYA: 'Scan the QR code below using your Maya app',
+  PAYMENT_PAYMENT_TIP: 'Your order will be processed immediately after payment confirmation.',
+
+  FINISH_THANK_YOU: 'Thank you For Your Order',
+  FINISH_ARRIVING_SOON: 'Your delicious coffee will arrive soon!',
+  FINISH_ESTIMATED_DELIVERY: 'Estimated Delivery : 20–30 mins',
+
+  TRACK_TRACK_SUB: 'You can now track the status of your delivery here',
+
+  LOGIN_DESCRIPTION: 'Buy Coffees, access your orders, track deliveries, and enjoy exclusive member perks.',
+  LOGIN_SIGN_IN: 'Sign in to continue',
+  LOGIN_TERMS: 'By continuing, you agree to our Terms & Privacy Policy.',
+  LOGIN_CONTINUE_FACEBOOK: 'Continue With Facebook',
+  LOGIN_CONTINUE_GOOGLE: 'Continue With Google',
 };
